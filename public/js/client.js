b4ed8735765b7ddcefb898aa8efaa2d10594fb4d
OusmaneMballo <ousmanemballo576@gmail.com>
2020-06-18 17:36:04 +0000

    Implementation des fonctions de validation de saisie

--- public/js/client.js
+++ public/js/client.js
@@ -25,3 +25,62 @@ function typeClient() {
         }
     }
 }
+
+//Fonction de verification des champs input d'un client moral
+function verificationMoral() {
+
+    if (document.getElementById("cm").disabled == false){
+       
+        if (document.getElementById('nomCM').value === '' || document.getElementById('adresseCM').value === '') {
+                return false;
+        }
+        if (document.getElementById('raisonSocialCM').value === '' || document.getElementById('emailCM').value === ''){
+            return false;
+        }
+        if (document.getElementById('loginCM').value === '' || document.getElementById('passwdCM').value === '') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//Fonction de verification des champs input d'un client physique
+function verificationPhysique() {
+
+    if (document.getElementById("cp").disabled == false) {
+
+        if (document.getElementById('nomcp').value === '' || document.getElementById('prenomcp').value === '') {
+            return false;
+        }
+        if (document.getElementById('telephonecp').value === '' || document.getElementById('salairecp').value === '') {
+            return false;
+        }
+        if (document.getElementById('emailcp').value === '' || document.getElementById('logincp').value === '') {
+            return false;
+        }
+        if (document.getElementById('passwdcp').value === '') {
+            return false;
+        }
+        if (document.getElementById('cnicp').value === '' || document.getElementById('adressecp').value === '') {
+            return false;
+        }
+        let client = document.getElementById("statutcp");
+        let choix = client.selectedIndex;
+        let statut = client.options[choix].value;
+        if (statut<=0){
+            return false;
+        }
+    }
+    return true;
+}
+//Fonction de validation des champs input
+function validation(){
+    
+    if(verificationMoral()==true && verificationPhysique()==true){
+        return alert('Okey tout les champs sont remplis')
+    }
+
+    alert('Merci de renseigner les champs actives');
+    return false;
+}
