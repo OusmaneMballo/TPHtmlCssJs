821969231d6fd92410566dc95969e4ba1ca02194
OusmaneMballo <ousmanemballo576@gmail.com>
2020-06-25 11:19:07 +0000

    Mise en place de l'architecture MVC

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     </header>
     <aside class="sidebarre">
         <div class="flex">
-            <img src="./img/profil.jpg" class="profil" alt="Banque du Peuple" srcset="">
+            <img src="./public/img/profil.jpg" class="profil" alt="Banque du Peuple" srcset=""/>
             <p class="mail">xywzt@gmail.com</p>
             <div class="contener" style="background-color: rgb(85, 163, 231); color: white;">
                 Dashboard
